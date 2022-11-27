5a413828cb2ebdfee1b76c7628d40556525fa800
MingganM <m4mew1@gmail.com>
2022-11-27 21:07:53 +0500

    V1.2.1 / added a dynamic birthday and fixed design.

--- src/script.js
+++ src/script.js
@@ -7,3 +7,10 @@ import './scripts/projectHandler';
 
 // navbar event listener
 selectors.navBtn.addEventListener('click', displayNav);
+// dynamic age
+let now = new Date();
+let DOB = new Date("2001,11,29");
+let ageYear = now.getFullYear() - DOB.getFullYear();
+let ageMonth = now.getMonth() - DOB.getMonth();
+let ageDay = now.getDate() - DOB.getDate();
+selectors.age.textContent = ageMonth <= 0 && ageDay <= 0 ? ageYear - 1 : ageYear ;
--- src/scripts/selector.js
+++ src/scripts/selector.js
@@ -1,6 +1,6 @@
 const selectors = {
     navBtn: document.querySelector('.nav__btn'),
     mobileNav: document.querySelector('.nav__mobile'),
-    
+    age: document.querySelector('.age')
 }
 export default selectors;
